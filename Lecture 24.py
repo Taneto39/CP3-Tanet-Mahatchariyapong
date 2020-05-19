3f9c5d1e157d161f91298a960844d8225a33a2f1
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2020-05-20 00:36:36 +0700

    New

---
+++ Lecture 24.py
@@ -0,0 +1,9 @@
+firstName = "Tanet"
+lastName = "Mahatchariyapong"
+age = 23
+height = 172
+weight = 67
+print("My name is", firstName, lastName+".")
+print("I'm", age, "year(s) old.")
+print("My height is", height, "cm.")
+print("My weight is", weight, "kg.")
