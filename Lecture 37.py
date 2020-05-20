9d3d7793de0d1c6f5b32c4beab2365b37a92500f
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2020-05-21 00:22:07 +0700

    Create Lecture 37.py

---
+++ Lecture 37.py
@@ -0,0 +1,7 @@
+userInput = input("Username : ")
+passInput = input("Password : ")
+if userInput == "admin" and passInput == "1234":
+    print("Welcome ", userInput)
+else:
+    print("ERROR !!")
+    
