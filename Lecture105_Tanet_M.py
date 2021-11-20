c141753fec858dbff59a0365eaaa0f5981c5ae0e
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2021-11-20 10:28:00 +0700

    reformat

--- Lecture105_Tanet_M.py
+++ Lecture105_Tanet_M.py
@@ -1,23 +1,32 @@
 class Vehicle:
     licenseCode = ""
     serialCode = ""
-    def turnOnAC(self):
+
+    def turnon_air(self):
         print("AC On!")
+
 
 class Car(Vehicle):
     pass
+
+
 class Pickup(Vehicle):
     pass
+
+
 class Van(Vehicle):
     pass
+
+
 class EstateCar(Vehicle):
     pass
+
 
 car1 = Car()
 pickup1 = Pickup()
 van1 = Van()
 estate1 = EstateCar()
-car1.turnOnAC()
-pickup1.turnOnAC()
-van1.turnOnAC()
-estate1.turnOnAC()
+car1.turnon_air()
+pickup1.turnon_air()
+van1.turnon_air()
+estate1.turnon_air()
