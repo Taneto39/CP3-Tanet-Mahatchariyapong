41eb09486d3cd44bc9b907f9e13a47b55eca01aa
Taneto39 <65605866+Taneto39@users.noreply.github.com>
2021-11-20 11:09:33 +0700

    configure function turn_on_air

--- Lecture105_Tanet_M.py
+++ Lecture105_Tanet_M.py
@@ -2,8 +2,8 @@ class Vehicle:
     licenseCode = ""
     serialCode = ""
 
-    def turnon_air(self):
-        print("AC On!")
+    def turn_on_air(self):
+        print(f"{self.licenseCode}'s AC On!")
 
 
 class Car(Vehicle):
@@ -23,10 +23,14 @@ class EstateCar(Vehicle):
 
 
 car1 = Car()
+car1.licenseCode = "Car"
 pickup1 = Pickup()
+pickup1.licenseCode = "Pickup"
 van1 = Van()
+van1.licenseCode = "Van"
 estate1 = EstateCar()
-car1.turnon_air()
-pickup1.turnon_air()
-van1.turnon_air()
-estate1.turnon_air()
+estate1.licenseCode = "Estate"
+car1.turn_on_air()
+pickup1.turn_on_air()
+van1.turn_on_air()
+estate1.turn_on_air()
