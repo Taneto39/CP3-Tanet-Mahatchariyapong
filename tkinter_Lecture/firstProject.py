554c72e86914b87240c2ed981e95c5446094db55
Taneto39 <tanet1997@hotmail.com>
2023-01-17 00:05:51 +0700

    begin new
    new simple program for homework

--- tkinter_Lecture/firstProject.py
+++ tkinter_Lecture/firstProject.py
@@ -1,51 +1,46 @@
-from tkinter import *
-
-root = Tk()
-currency = [str(i) for i in range(10, 22)]
-primary_currency = ""
-secondary_currency = []
+import tkinter as tk
+from tkinter import ttk
+from currency_converter import CurrencyConverter
+from datetime import date
 
 
-def pri_choose():
-    global primary_currency
-    primary_currency = currency[pri_cur.get()]
-    currency.remove(primary_currency)
-    show_sec_cur()
-
-
-def show_sec_cur():
-    for i in range(12):
-        globals()[f'rBtn{i}'].destroy()
-    btn_pri_receive.configure(text='plot', command=save_sec_cur)
-    Label(root, text=f'{primary_currency}').grid(row=3, column=2)
-    for index, ele in enumerate(currency):
-        globals()[f'check{index}'] = IntVar()
-        Checkbutton(root, text=currency[index], variable=globals()[f'check{index}']).grid(row=index // 4,
-                                                                                          column=index % 4)
-
-
-def save_sec_cur():
-    global secondary_currency
-    for i in range(11):
-        secondary_currency.append(globals()[f'check{i}'].get())
-    index_cur = secondary_currency.copy()
-    secondary_currency.clear()
-    for index, value in enumerate(index_cur):
-        if value == 1:
-            secondary_currency.append(currency[index])
-    time_get()
-
-
-def time_get():
+def test():
     pass
 
 
-pri_cur = IntVar()
-for idx, cur in enumerate(currency):
-    globals()[f'rBtn{idx}'] = Radiobutton(root, text=cur, variable=pri_cur, value=idx)
-    globals()[f'rBtn{idx}'].grid(row=idx // 4, column=idx % 4)
-Label(root, text=f'Primary').grid(row=3, column=0)
-Label(root, text=f'Currency').grid(row=3, column=1)
-btn_pri_receive = Button(root, text="OK", command=pri_choose)
-btn_pri_receive.grid(row=3, column=3)
-root.mainloop()
+# provide list of currencies
+c = CurrencyConverter()
+lst_currency = list(c.currencies)
+lst_currency.sort()
+
+window = tk.Tk()
+window.rowconfigure([0], minsize=100)
+window.rowconfigure([1], minsize=50)
+window.columnconfigure([0], minsize=210)
+window.resizable(False, False)
+
+frm_program = tk.Frame(window, borderwidth=1, relief=tk.RAISED)
+frm_program.rowconfigure([0, 1], minsize=50)
+frm_program.columnconfigure([0, 1, 2], minsize=100)
+frm_program.grid(row=0, column=0, sticky='nsew')
+
+# first row widget
+input_currency_var = tk.StringVar()
+lbox_input_currency = ttk.Combobox(frm_program, textvariable=input_currency_var, values=lst_currency, state='readonly')
+lbox_input_currency.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
+
+btn_alt_currency = tk.Button(frm_program, text='\u21c4')
+btn_alt_currency.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
+
+output_currency_var = tk.StringVar()
+lbox_output_currency = ttk.Combobox(
+    frm_program,
+    textvariable=output_currency_var,
+    values=lst_currency,
+    state='readonly')
+lbox_output_currency.grid(row=0, column=2, padx=5, pady=5, sticky='nsew')
+
+# second row widget
+ent_input_value = tk.Entry(frm_program, )
+
+window.mainloop()
