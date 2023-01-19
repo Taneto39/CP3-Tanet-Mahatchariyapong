233e1c89a21d04f1b4462694eb41800ac484aed4
Taneto39 <tanet1997@hotmail.com>
2023-01-19 23:25:29 +0700

    finished
    -rename
    -add comment

--- tkinter_Lecture/SimpleCurrencyConverter.py
+++ tkinter_Lecture/SimpleCurrencyConverter.py
@@ -1,42 +1,50 @@
 import tkinter as tk
 from tkinter import ttk
 from currency_converter import CurrencyConverter
-# from datetime import date
 import re
 
 
+# Entry can type only digits
 def check_num(new_val):
     return re.match('^[0-9]*$', new_val) is not None and len(new_val) <= 10
 
 
+# callback for alternate button
 def alternate_currency():
-    # result_var.set('35')
-    input_cur = lbox_input_currency.get()
-    output_cur = lbox_output_currency.get()
-    lbox_input_currency.set(output_cur)
-    lbox_output_currency.set(input_cur)
-    input_value = ent_input_value.get()
-    output_value = result_var.get()
+    # alternate currency in combobox
+    input_cur = cbx_input_currency.get()
+    output_cur = cbx_output_currency.get()
+    cbx_input_currency.set(output_cur)
+    cbx_output_currency.set(input_cur)
+    # clear value for prevent misunderstand
     ent_input_value.delete(0, tk.END)
-    ent_input_value.insert(0, output_value)
-    result_var.set(input_value)
+    lbl_result['text'] = ''
 
 
-def convert():
-    pass
+# clear value for prevent misunderstand
+def clear_value(e):
+    ent_input_value.delete(0, tk.END)
+    lbl_result['text'] = ''
+
+
+# callback when user press enter
+def convert(e):
+    input_value = float(ent_input_value.get())
+    result = c.convert(input_value, cbx_input_currency.get(), cbx_output_currency.get())
+    lbl_result['text'] = round(result, 2)
 
 
 # provide list of currencies
-c = CurrencyConverter()
+c = CurrencyConverter(fallback_on_wrong_date=True)
 lst_currency = list(c.currencies)
 lst_currency.sort()
 
 window = tk.Tk()
 window.rowconfigure([0], minsize=100)
-# window.rowconfigure([1], minsize=50)
 window.columnconfigure([0], minsize=210)
 window.resizable(False, False)
 check_num_wrapper = (window.register(check_num), '%P')
+window.title('Simple Currency Converter')
 
 frm_program = tk.Frame(window, borderwidth=1, relief=tk.RAISED)
 frm_program.rowconfigure([0, 1], minsize=50)
@@ -45,27 +53,29 @@ frm_program.grid(row=0, column=0, sticky='nsew')
 
 # first row widget
 input_currency_var = tk.StringVar()
-lbox_input_currency = ttk.Combobox(
+cbx_input_currency = ttk.Combobox(
     frm_program,
     textvariable=input_currency_var,
     values=lst_currency,
     state='readonly',
     width=10,
     font=40)
-lbox_input_currency.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
+cbx_input_currency.grid(row=0, column=0, padx=5, pady=5, sticky='nsew')
+cbx_input_currency.bind('<<ComboboxSelected>>', clear_value)
 
 btn_alt_currency = tk.Button(frm_program, text='\u21c4', command=alternate_currency)
 btn_alt_currency.grid(row=0, column=1, padx=5, pady=5, sticky='nsew')
 
 output_var = tk.StringVar()
-lbox_output_currency = ttk.Combobox(
+cbx_output_currency = ttk.Combobox(
     frm_program,
     textvariable=output_var,
     values=lst_currency,
     state='readonly',
     width=10,
     font=40)
-lbox_output_currency.grid(row=0, column=2, padx=5, pady=5, sticky='nsew')
+cbx_output_currency.grid(row=0, column=2, padx=5, pady=5, sticky='nsew')
+cbx_output_currency.bind('<<ComboboxSelected>>', clear_value)
 
 # second row widget
 input_var = tk.StringVar()
@@ -77,15 +87,13 @@ ent_input_value = tk.Entry(
     width=10,
     font=40)
 ent_input_value.grid(row=1, column=0, padx=5, pady=5, sticky='nsew')
-ent_input_value.bind()
+ent_input_value.bind('<KeyPress-Return>', convert)
 
 lbl_equal = tk.Label(frm_program, text=':', font=40)
 lbl_equal.grid(row=1, column=1, padx=5, pady=5, sticky='nsew')
 
-result_var = tk.StringVar()
 lbl_result = tk.Label(frm_program,
-                      textvariable=result_var,
-                      bg='yellow',
+                      text='',
                       font=40)
 lbl_result.grid(row=1, column=2, padx=5, pady=5, sticky='nsew')
 
